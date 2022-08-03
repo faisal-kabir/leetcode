45fe3f5b0bf75967c33411d90e6c37889fc214b6
Faisal <faisalkabir95@gmail.com>
2022-08-03 11:16:37 +0600

    Solved 392 problem

---
+++ Is_Subsequence_392.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Is_Subsequence_392 {
+
+    public static void main(String args[]) {
+        String s = "abc", t = "ahbgdc";
+        System.out.println(isSubsequence(s, t));
+    }
+
+    /**
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isSubsequence(String s, String t) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int sequenceIndex = 0;
+        for(int i=0; i<t.length(); i++){
+            if(t.charAt(i) == s.charAt(sequenceIndex)){
+                sequenceIndex ++;
+                if(s.length() == sequenceIndex){
+                    break;
+                }
+            }
+        }
+        return sequenceIndex == s.length();
+    }
+}
