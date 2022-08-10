1863e4fd26e702d4e8842c7acab2c44d730215fa
Faisal <faisalkabir95@gmail.com>
2022-08-10 15:06:09 +0600

    Solved 260 problem

--- Single_Number_136.java
+++ Single_Number_136.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Single_Number_136 {
     public static void main(String args[]) {
-        System.out.println(singleNumber(new int []{4,1,2,1,2}));
+        System.out.println(singleNumberIII(new int []{4,1,2,1,2}));
+        System.out.println(singleNumberIII(new int []{1,2,1,3,2,5}));
     }
 
 
@@ -16,5 +22,21 @@ public class Single_Number_136 {
             }
        }
        return (int) couple.keySet().toArray()[0];
+    }
+    public static int[] singleNumberIII(int[] nums) {
+        Set<Integer> setOfInteger = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+             if(setOfInteger.contains(nums[i])){
+                setOfInteger.remove(nums[i]);
+             } else {
+                setOfInteger.add(nums[i]);
+             }
+        }
+        return  convertIntSetToStringSet(setOfInteger);
+    }
+    public static int[] convertIntSetToStringSet(Set<Integer> setOfInteger){
+        return setOfInteger.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
