72f91f05903c118a5be69f42f2a8e7cf42d21b17
Faisal <faisalkabir95@gmail.com>
2022-08-10 16:06:55 +0600

    Add

---
+++ Reverse_Vowels_of_a_String_345.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Reverse_Vowels_of_a_String_345 {
+    public static void main(String args[]) {
+        System.out.println(reverseVowels("leetcode"));
+    }
+
+    public static String reverseVowels(String s) {
+        Set<String> setOfInteger = new HashSet<>(Arrays.asList("a", "A", "e", "E", "i", "I", "o", "O", "u", "U"));
+        int end = s.length()-1;
+        for(int i=0; i<s.length(); i++){
+            
+        }
+        return "hullen";
+    }
+}
--- Single_Number_136.java
+++ Single_Number_136.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Single_Number_136 {
