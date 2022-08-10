20696c9f6238ebc67cb6f13f7b956a39ba9f6f25
Faisal <faisalkabir95@gmail.com>
2022-08-10 10:43:03 +0600

    Solved 142 and 141 problem

--- Linked_List_Cycle_II_142.java
+++ Linked_List_Cycle_II_142.java
@@ -1,25 +1,37 @@
+import java.util.*;
+
 public class Linked_List_Cycle_II_142 {
     
     public static void main(String args[]) {
-        System.out.println(detectCycle(new ListNode(3,new ListNode(2,new ListNode(0,new ListNode(-4))))));
+        int[] keys = {-1,-7,7,-4,19,6,-9,-5,-2,-5};
+ 
+        ListNode head = null;
+        for (int i = keys.length - 1; i >= 0; i--) {
+            head = new ListNode(keys[i], head);
+        }
+        head.next.next.next.next.next.next.next.next.next.next = head.next.next.next.next.next.next;
+
+        System.out.println(detectCycle(head));
     }
 
 
     public static ListNode detectCycle(ListNode head) {
-        if(head.next == null){
-            return head.next;
-        }
         ListNode current = head;
-        int count = 0;
-        while(current != null){
-            count++;
+        Set<ListNode> set = new HashSet<>();
+    
+        while (current != null)
+        {
+            if(set.contains(current)){
+                System.out.println("Loop = "+current.val);
+                return current;
+            }
+            
+            set.add(current);
             current = current.next;
         }
-        if(count%2 == 0){
-            head = head.next;
-        }
+ 
     
-        return head;
+        return null;
     }
 
 }
