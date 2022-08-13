632f777234edfa08df99ef70df44eea551a84c48
Faisa Kabir <faisalkabir95@gmail.com>
2022-08-13 23:43:49 +0600

    Solved 345 problem

--- Reverse_Vowels_of_a_String_345.java
+++ Reverse_Vowels_of_a_String_345.java
@@ -1,38 +1,55 @@
 import java.util.*;
-import java.util.regex.Pattern;
 public class Reverse_Vowels_of_a_String_345 {
     public static void main(String args[]) {
+        //System.out.println(reverseVowels(" "));
+        System.out.println(reverseVowels("ab"));
         //System.out.println(reverseVowels("hello"));
         //System.out.println(reverseVowels("leetcode"));
-        System.out.println(reverseVowels("race a car"));//"raca e car"
-        System.out.println(reverseVowels("Sore was I ere I saw Eros."));//"SorE was I ere I saw eros."
+        //System.out.println(reverseVowels("race a car"));//"raca e car"
+        //System.out.println(reverseVowels("Sore was I ere I saw Eros."));//"SorE was I ere I saw eros."
+        //System.out.println(reverseVowels("Marge, let's \"went.\" I await news telegram."));
+        //System.out.println(reverseVowels("Go hang a salami, I'm a lasagna hog.")); //"Go hang a salamI, i'm a lasagna hog."
     }
 
     public static String reverseVowels(String s) {
+        if(s==null || s.isEmpty() || s==" "){
+            return s;
+        }
         Set<String> vowels = new HashSet<>(Arrays.asList("a", "A", "e", "E", "i", "I", "o", "O", "u", "U"));
         Set<String> special = new HashSet<>(Arrays.asList("!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"));
         StringBuilder copy = new StringBuilder(s);
     
         int end = copy.length()-1;
-        for(int i = copy.length()%2 == 0 ? 0 : 1; i <= copy.length()/2; i++){
-            if(copy.charAt(i) == ' '){
+        int i=0;
+        while(true){
+            if(i >= copy.length() || end<=0){
+                break;
+            }
+            if(copy.charAt(i) == ' ' || special.contains(copy.charAt(i)+"")){
                 i++;
                 continue;
             }
             
             if(copy.charAt(end) == ' ' || special.contains(copy.charAt(end)+"")){
                 end--;
-                i--;
                 continue;
             }
-            //System.out.print(copy.charAt(i)+" - "+copy.charAt(end)+" "+i);
-            //System.out.print(" "+vowels.contains(copy.charAt(i)+"")+" "+end);
+            if(end<i){
+                break;
+            }
+            System.out.println("Left = "+copy.charAt(i)+" / Right = "+copy.charAt(end)+" / start = "+i+" / End = "+end+" / "+(vowels.contains(copy.charAt(i)+"") && vowels.contains(copy.charAt(end)+"")));
             if(vowels.contains(copy.charAt(i)+"") && vowels.contains(copy.charAt(end)+"")){
                 char temp = copy.charAt(i);
                 copy.setCharAt(i,copy.charAt(end));
                 copy.setCharAt(end,temp);
+                i++;
+                end--;
+                System.out.println(copy);
+            } else if(!vowels.contains(copy.charAt(i)+"")){
+                i++;
+            } else if(!vowels.contains(copy.charAt(end)+"")){
+                end--;
             }
-            end--;
         }
         return copy.toString();
     }
