c25938fd69745cff1ac0fd125a4e61e5e33a1a73
Faisal <faisalkabir95@gmail.com>
2022-08-24 11:32:22 +0600

    Solved 98 problem

--- Validate_Binary_Search_Tree_98.java
+++ Validate_Binary_Search_Tree_98.java
@@ -1,43 +1,25 @@
 public class Validate_Binary_Search_Tree_98 {
 
     public static void main(String args[]) {
-        isValidBST(TreeNode.createTree(new Integer[]{32,26,47,19,null,null,56,null,27}));
+        System.out.println("Is valid = "+isBST(TreeNode.createTree(new Integer[]{32,26,47,19,null,null,56,null,27}),null,null));
     }
-    static boolean valid = true;
-    public static boolean isValidBST(TreeNode root) {
-        if(root == null){
-            return false;
-        } else if (root.left == null && root.right == null){
+    static boolean isBST(TreeNode node,Integer low, Integer high) {
+        if (node == null)
             return true;
-        }
+        
+        if (low != null && node.val <= low)
+            return false;
+        
+        if (high!=null && node.val >= high)
+            return false;
+        
+        if (!isBST(node.right,node.val,high))
+            return false;
 
-        nextNode(root,root.val,0,null,null);
-        return valid;
-    }
-
-    public static void nextNode(TreeNode node,int headValue,int lastValue,Boolean isLeft,Boolean isHeadLeft){
-        System.out.println(node.val + " isLeft = "+isLeft+" / isHeadLeft = "+isHeadLeft+" / Last Value = "+lastValue);
-        if(isHeadLeft!=null){
-            if(isHeadLeft && headValue<node.val){
-                valid = false;
-            } else if(!isHeadLeft && headValue>node.val){
-                valid = false;
-            } else if(isLeft && node.val > lastValue){
-                valid = false;
-            } else if(!isLeft && node.val < lastValue){
-                valid = false;
-            }
-        }
-
-        if(valid){
-            if (node.left != null){
-                nextNode(node.left,headValue,node.val,true,isHeadLeft!=null ? isHeadLeft : true);
-            }
-
-            if (node.right != null){
-                nextNode(node.right,headValue,node.val,false,isHeadLeft!=null ? isHeadLeft : false);
-            }
-        }
+        if (!isBST(node.left,low,node.val))
+            return false;
+        
+        return true;
     }
 
 }
